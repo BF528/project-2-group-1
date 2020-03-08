35ea0c0c767e4f9559c86e30de5d5d9d84e113b8
Garima <48873678+garimaloh@users.noreply.github.com>
2020-03-08 18:19:09 -0400

    Update Project2_Analyst.R

--- Analyst/Project2_Analyst.R
+++ Analyst/Project2_Analyst.R
@@ -1,5 +1,5 @@
 data <- read.table('gene_exp.diff',header=T,as.is=T)  # reading gene_exp.diff file into R
-hist(data$log2.fold_change.,breaks=100)   # plotting histogram of log2.fold_change for all genes
+hist(data$log2.fold_change.,breaks=100,main = "Histogram of Log2 fold change for all genes",xlab = "Log2 fold change",col = "blue") # plotting histogram of log2.fold_change for all genes
 data <- data[order(data$q_value),]        # rearranging data in order of q value
 dim(data)#36329                     
 dev.copy(pdf,'all_fold_change.pdf')       # saving the plot
@@ -7,7 +7,7 @@ dev.off()
 head(data,n=10)
 
 data.sig <- subset(data,significant=="yes")  # subsetting significant genes from all the genes
-hist(data.sig$log2.fold_change.,breaks=100)  # plotting histogram of log2.fold_change for differentially expressed genes
+hist(data.sig$log2.fold_change.,breaks=100,main = "Histogram of Log2 fold change for significant DE genes",xlab = "Log2 fold change",col = "blue") # plotting histogram of log2.fold_change for differentially expressed genes
 dev.copy(pdf,'sig_fold_change.pdf')          # saving the plot
 dev.off()
 
